71f95edab2575e07111b76fa863ad92f97ee06f7
migueldeoleiros <migueldeoleiros@gmail.com>
2022-06-19 16:46:55 +0200

    base de cuenta y recibo en pedir y pagar

--- GEI-DS-614G010152122-OP2/src/tpv/estadosComanda/Pedir.java
+++ GEI-DS-614G010152122-OP2/src/tpv/estadosComanda/Pedir.java
@@ -2,7 +2,10 @@ package tpv.estadosComanda;
 
 import tpv.Comanda;
 import tpv.Estado;
-import tpv.Producto;
+import tpv.productos.ProductoVenta;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Pedir implements Estado {
     private static final Pedir instancia = new Pedir();
@@ -10,15 +13,35 @@ public class Pedir implements Estado {
     public static Pedir getInstance() {return instancia;}
 
     @Override
-    public String pedir(Comanda c, Producto pedido, int cantidad) {
-        if(c.getDespensa().removeProducto(pedido, cantidad))
-            c.getPedidos().put(pedido, cantidad);
-        return null;
+    public boolean pedir(Comanda c, ProductoVenta pedido) {
+        if(c.getDespensa().decrementarProducto(pedido, pedido.getCantidad())){
+            c.getPedidos().add(pedido);
+            return true;
+        }
+        return false;
     }
 
     @Override
     public String solicitarCuenta(Comanda c) {
+        StringBuilder output = new StringBuilder();
+        float total=0;
+        float totalNoImpuestos=0;
+
+        output.append("# Mesa numero ").append(c.getMesa()).append("\n");
+        output.append("# ").append(new SimpleDateFormat("dd/MM/yyyy HH:mm").format(new Date())).append("\n");
+        output.append("Producto\t Cantidad \t Precio \t PVP unidad \t PVP total \n");
+        output.append("===========================================================");
+        for(ProductoVenta i : c.getPedidos()){
+            output.append(i.toString()).append("\n");
+            totalNoImpuestos += i.getPrecio();
+            total += (i.getPrecio()+i.getPrecio()*i.getImpuestos());
+        }
+        output.append("# Pendiente de combro\n");
+        output.append("Total sin impuestos ").append(totalNoImpuestos).append("\n");
+        output.append("Total de impuestos ").append(total-totalNoImpuestos).append("\n");
+        output.append("PVP impuestos ").append(total).append("\n");
+
         c.setEstado(Cobro.getInstance());
-        return null;
+        return output.toString();
     }
 }
--- GEI-DS-614G010152122-OP2/src/tpv/metodosPago/PagoEfectivo.java
+++ GEI-DS-614G010152122-OP2/src/tpv/metodosPago/PagoEfectivo.java
@@ -2,6 +2,10 @@ package tpv.metodosPago;
 
 import tpv.Comanda;
 import tpv.MetodoPago;
+import tpv.productos.ProductoVenta;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class PagoEfectivo implements MetodoPago {
     @Override
@@ -10,6 +14,25 @@ public class PagoEfectivo implements MetodoPago {
     }
 
     public String pagar(Comanda c, double descuento) {
-        return null;
+        StringBuilder output = new StringBuilder();
+        float total=0;
+        float totalNoImpuestos=0;
+
+        output.append("# Factura simplificada numero ").append(c.hashCode()).append("\n");
+        output.append("# Mesa numero ").append(c.getMesa()).append("\n");
+        output.append("# ").append(new SimpleDateFormat("dd/MM/yyyy HH:mm").format(new Date())).append("\n");
+        output.append("Producto\t Cantidad \t Precio \t PVP unidad \t PVP total \n");
+        output.append("===========================================================");
+        for(ProductoVenta i : c.getPedidos()){
+            output.append(i.toString()).append("\n");
+            totalNoImpuestos += i.getPrecio();
+            total += (i.getPrecio()+i.getPrecio()*i.getImpuestos());
+        }
+        output.append("# Total\n");
+        output.append("Total sin impuestos ").append(totalNoImpuestos).append("\n");
+        output.append("Total de impuestos ").append(total-totalNoImpuestos).append("\n");
+        output.append("PVP impuestos ").append(total).append("\n");
+
+        return output.toString();
     }
 }
